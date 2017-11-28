34b00eb72c146d93d2d2ebb281c0dec1329eca06
Epica-Lolz <cat.nyane@gmail.com>
2017-11-28 15:49:54 -0500

    minor changes to the kernel and getchar
    + added getchar()
    + added inb/outb()
    --TODO--
    * Add EOF support to getchar()
    * Add proper keyboard correspondance ('a' pressed, 'a' printed).

--- kernel01-2d/kernel/kernel/kernel01-2d.c
+++ kernel01-2d/kernel/kernel/kernel01-2d.c
@@ -4,17 +4,17 @@
 #include <kernel/tty.h>
 
 void print_scroller(){
-    int counter=0;
-    int nl_ctr=0;
-    char point[] = "SkillsUSA 01-2d\nMeme\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23";
-    printf(point);
-    while(point[counter]!='\0'){
-        counter++;
-        if(point[counter]=='\n'){
-            nl_ctr++;
-            if(nl_ctr>=26){
-                term_scroll();
-                nl_ctr-=1;
+    int counter=0;//index variable to scroll through all characters of the array
+    int nl_ctr=0;//index variable for number of newlines
+    char point[] = "SkillsUSA 01-2d\nMeme\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n";//char array
+    printf(point);//print(char-array)
+    while(point[counter]!='\0'){//while the array is not empty
+        counter++;//count all characters of the array
+        if(point[counter]=='\n'){//if the character is a newline
+            nl_ctr++;//increment number of newlines
+            if(nl_ctr>=26){//if this number is larger than 26 (max number of columns for vga)
+                term_scroll();//scroll the terminal
+                nl_ctr-=1;//decrement the newline counter (because it only counts the number of on-screen newlines
             }
         }
     }
@@ -22,5 +22,9 @@ void print_scroller(){
 
 void kmain(void){
     term_init();//call term_init function defined in tty.c
-    print_scroller();
+    print_scroller();//call print_scroller
+    for(;;){
+        char ch=getchar();
+        printf("%c", ch);
+    }
 }
